62a6f1185f3ad1f06e7e7f9d9ea3bcbae9c650e3
tanviruman <ruman777ahmed@gmail.com>
2024-10-04 09:26:01 +0600

    Add ArrayList module to project

---
+++ ArrayList/src/ArrayListExample1.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+public class ArrayListExample1 {
+    public static void main(String[] args) {
+ArrayList<String> list = new ArrayList<>();
+list.add("A");
+list.add("B");
+list.add("C");
+        System.out.println(list);
+
+    }
+
+}
---
+++ ArrayList/src/ArrayListExample3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class ArrayListExample3 {
+    public static void main(String[] args) {
+        ArrayList <String> list = new ArrayList<>();
+        list.add("Tanvir");
+        list.add("Ahmed");
+        list.add("Ruman");
+
+        for(String Name:list){
+
+            System.out.println(Name);
+        }
+
+    }
+}
