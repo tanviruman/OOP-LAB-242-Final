a5f4bd9c380b214a9a3b8cb9ef022760a227976f
tanviruman <ruman777ahmed@gmail.com>
2024-10-04 11:20:31 +0600

    Refactor Example6 and Example7 classes in the String module

--- String/src/Example6.java
+++ String/src/Example6.java
@@ -1,2 +1,16 @@
 public class Example6 {
+    public static void main(String[] args) {
+        String S1 = "Tanvir";
+        String S2 = "Tanvir";
+        String S3 = "Ahmed";
+        System.out.println(S1.compareTo(S2));
+        System.out.println(S1.compareTo(S3));
+        System.out.println(S3.compareTo(S1));
+        String s1="Sachin";
+        String s2="Sachin";
+        String s3="Tanvir";
+        System.out.println(s1.compareTo(s2));//0
+        System.out.println(s1.compareTo(s3));//1(because s1>s3)
+        System.out.println(s3.compareTo(s1));//-1(because s3 < s1 )
+    }
 }
--- String/src/Example7.java
+++ String/src/Example7.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Example7 {
+public class Example7 {
+    public static void main(String[] args) {
+        String S1= "TanvirAhmedRuman";
+        System.out.println(S1.substring(7));
+        System.out.println(S1.substring(1,10));
+    }
 }
