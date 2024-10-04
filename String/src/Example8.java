49c47a6ce1b2bee5fd660694f17b0e39248e920c
tanviruman <ruman777ahmed@gmail.com>
2024-10-04 14:08:49 +0600

    Add Example8 class with string split method

--- String/src/Example8.java
+++ String/src/Example8.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Example8 {
+public class Example8 {
+
+    public static void main(String[] args) {
+
+        String S = "java string split method by javatpoint";
+         String  w[] =  S.split(" ");
+
+         for( String words:w){
+
+             System.out.println(words);
+         }
+
+
+    }
+
+
+
 }
