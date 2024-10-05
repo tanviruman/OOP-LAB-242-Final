282959340e709fba7b6878eeecc04f087d298dd9
tanviruman <ruman777ahmed@gmail.com>
2024-10-05 18:35:17 +0600

    Add p_Equality class with string comparison functionality

--- ClassCode/src/p_Equality.java
+++ ClassCode/src/p_Equality.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class p_Equality {
+import java.util.Scanner;
+public class p_Equality {
+    public static void main(String[] args) {
+        String S1="Tanvir Ahmed";
+        Scanner sc = new Scanner(System.in);
+
+        for (int i=0;i<12;i++){
+
+            String data = sc.next();
+
+            int res = S1.compareTo(data);
+            if(res>0){
+                System.out.println(res);
+                System.out.println("S1<data");
+            }
+            if(res<0){
+                System.out.println(res);
+                System.out.println("S1>data");
+
+            }
+            else{
+                System.out.println("equal");
+            }
+        }
+    }
 }
