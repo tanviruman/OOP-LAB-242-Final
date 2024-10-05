312542edf673a897da411d82228ba059b7fabeb5
tanviruman <ruman777ahmed@gmail.com>
2024-10-05 18:11:05 +0600

    Add ClassCode and ClassString modules and source files

--- ClassCode/src/P_Container.java
+++ ClassCode/src/P_Container.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class P_Container {
+public class P_Container {
+    public static void main(String[] args) {
+
+        String S1 ="Abdcbbfjgvhegfkjbnabsyfwf";
+        String S2 = "bna";
+        boolean b = S1.contains(S2);
+        System.out.println(b);
+
+        String S3 =S1.substring(3);
+        System.out.println(S3);
+        String S4 = S1.substring(3,8);
+        System.out.println(S4);
+
+        System.out.println(S1.substring(3,8));
+        char[] s = S1.toCharArray();
+        System.out.println(s);
+    }
 }
