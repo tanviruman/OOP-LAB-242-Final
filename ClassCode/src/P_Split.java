d0822e7e16e8b53fa1a4ede668cbc5012a5f256f
tanviruman <ruman777ahmed@gmail.com>
2024-10-05 18:58:03 +0600

    Add P_Split and p_Class classes to ClassCode module

--- ClassCode/src/P_Split.java
+++ ClassCode/src/P_Split.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class P_Split {
+public class P_Split {
+
+
+    public static void main(String[] args) {
+        String S1 = " Tanvir,Ahmed,Ruman,009,0112320065,10,15,20";
+
+        String[] parts = S1.split(",");
+        for(String sc:parts){
+            System.out.println(sc);
+        }
+    }
 }
--- ClassCode/src/p_Class.java
+++ ClassCode/src/p_Class.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class p_Class {
+import java.util.Scanner;
+
+public class p_Class {
+
+    String name;
+    int id;
+
+    float cgpa;
+
+    public p_Class(String name, int id, float cgpa) {
+        this.name = name;
+        this.id = id;
+        this.cgpa = cgpa;
+    }
+    void display(){
+        System.out.println("Name"+name);
+        System.out.println("ID: " + id);
+        System.out.println("cgpa: " + cgpa);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String[] parts = s.split(",");
+        p_Class   st = new p_Class (parts[0],Integer.parseInt(parts[1]),Float.parseFloat(parts[2]));
+        st.display();
+    }
 }
